783c422c4931c8a5ba1b034249bffb4eab2ef63f
Leonardo Núñez <jorgebike.75@hotmail.com>
2025-07-19 09:02:59 -0300

    Cadena evolutiva
    - Se creó un nuevo componente llamado `MasInformacion.vue`.
    - Muestra la **cadena evolutiva** del Pokémon actual.
    - Incluye imágenes, nombres y flechas entre evoluciones.

---
+++ src/components/MasInformacion.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="masInformacion" v-if="evoluciones.length > 0">
+    <h2 class="tituloMasInformacion">Cadena evolutiva</h2>
+    <div class="contenedorEvoluciones">
+      <template v-for="(pokemon, index) in evoluciones" :key="pokemon.id">
+        <div class="pokemonEvolucion">
+          <img
+            :src="obtenerImagen(pokemon.id)"
+            :alt="pokemon.nombre"
+          />
+          <span>{{ pokemon.nombre }}</span>
+        </div>
+        <span class="flecha" v-if="index !== evoluciones.length - 1">→</span>
+      </template>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MasInformacion",
+  props: {
+    evoluciones: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    obtenerImagen(id) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/assets/lol.scss";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,61 +6,95 @@
         type="text"
         id="pokemonInput"
         v-model="pokemonID"
-        @keyup.enter="searchPokemon"
+        @keyup.enter="buscarPokemon"
       />
     </label>
-    <button class="searchButton" @click="searchPokemon">Buscar pokemon!</button>
+    <button class="searchButton" @click="buscarPokemon">Buscar Pokémon!</button>
   </header>
-
-  <main class="main" v-if="Object.entries(pokemonData).length > 0">
+  <main class="main" v-if="Object.entries(datosPokemon).length > 0">
     <section class="pokemonCard">
       <div class="nameImage">
-        <h1 class="pokemonName">#{{ pokemonData.id }} - {{ pokemonData.name }}</h1>
-        <img :src="pokemonData.sprites.front_default" :alt="pokemonData.name" />
+        <h1 class="pokemonName">
+          #{{ datosPokemon.id }} - {{ datosPokemon.name }}
+        </h1>
+        <img
+          :src="datosPokemon.sprites.front_default"
+          :alt="datosPokemon.name"
+        />
       </div>
       <ul class="type">
         <h2>Tipo:</h2>
         <li
-          v-for="(type, key) in pokemonData.types"
-          :key="key"
-          :class="type.type.name"
+          v-for="(tipo, clave) in datosPokemon.types"
+          :key="clave"
+          :class="tipo.type.name"
         >
-          <span>{{ type.type.name }}</span>
+          <span>{{ tipo.type.name }}</span>
         </li>
       </ul>
       <ul class="stats">
         <h2 class="statsH2">Estadísticas:</h2>
-        <li v-for="(stat, key) in pokemonData.stats" :key="key">
+        <li v-for="(stat, clave) in datosPokemon.stats" :key="clave">
           <span>{{ stat.stat.name }} -> {{ stat.base_stat }}</span>
         </li>
       </ul>
     </section>
+    <MasInformacion :evoluciones="cadenaEvolutiva" />
   </main>
 </template>
 
 <script>
 import axios from "axios";
 import { pokeapi } from "@/api/pokeapi";
+import MasInformacion from "@/components/MasInformacion.vue";
 
 export default {
-  name: "App",
+  name: "HomeView",
+  components: {
+    MasInformacion,
+  },
   data() {
     return {
-      pokemonData: {},
+      datosPokemon: {},
       pokemonID: "",
+      cadenaEvolutiva: [],
     };
   },
-
   methods: {
-    async searchPokemon() {
+    async buscarPokemon() {
       try {
         const respuesta = await axios.get(`${pokeapi}/${this.pokemonID}`);
-        this.pokemonData = respuesta.data;
-        console.log(respuesta.data);
-        return respuesta.data;
+        this.datosPokemon = respuesta.data;
+
+        const especie = await axios.get(respuesta.data.species.url);
+        const cadena = await axios.get(especie.data.evolution_chain.url);
+
+        this.cadenaEvolutiva = this.obtenerCadenaEvolutiva(cadena.data.chain);
       } catch (error) {
-        alert("Pokemon was not found");
+        alert("No se encontró el Pokémon");
+        this.datosPokemon = {};
+        this.cadenaEvolutiva = [];
       }
+    },
+
+    obtenerCadenaEvolutiva(cadena) {
+      const evoluciones = [];
+      let actual = cadena;
+
+      while (actual) {
+        evoluciones.push({
+          nombre: actual.species.name,
+          id: this.obtenerIDDesdeURL(actual.species.url),
+        });
+        actual = actual.evolves_to[0];
+      }
+
+      return evoluciones;
+    },
+
+    obtenerIDDesdeURL(url) {
+      const partes = url.split("/");
+      return partes[partes.length - 2];
     },
   },
 };
